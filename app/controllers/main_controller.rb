183e2feb2430afb24245b604012422122021f38b
Rkha21 <rkha02134@gmail.com>
2025-01-27 20:54:35 -0600

    Inital links

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,20 @@
+class MainController < ApplicationController
+    def index
+      @books = ['books1', 'books2', 'books3']
+    end
+  
+    def add
+      
+    end
+  
+    def update
+  
+    end
+  
+    def show
+    end
+  
+    def delete
+    end
+  end
+  
